acfd014ec5d0388429f503558a5749ea3e898a0e
Ellie Lockhart <69021568+EllieLockhart@users.noreply.github.com>
2020-08-27 16:58:44 -0500

    Switch to Jupyter notebook implementation to resolve mess

---
+++ python-implementation/initialize-imdb-data.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import numpy as np #initialize basic Python math data functions
+import pandas as pd #import the Pandas data library to create dataframes
+
+init_imdb = pd.read_csv("name.basics.tsv")
+init_imdb.head()
