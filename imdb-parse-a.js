3e3d3019288d76c17dfbd88d9fbe5a071e7e315c
Ellie Lockhart <69021568+EllieLockhart@users.noreply.github.com>
2020-08-27 15:23:50 -0500

    big mess
    a mess

--- imdb-parse-a.js
+++ imdb-parse-a.js
@@ -1,6 +1,12 @@
-var app = new Vue({ 
-    el: '#ShowData',
-    data: {
-        ConsoleRender: 'Vue Test Case'
+// script fetches and parses the data for the first stage of processing, without merging anything
+d3.tsv{"name.basics.tsv"), function(data) {
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[1].titleId);
     }
 });
+    }
+}
+
+// display console output into the DOM for debug purposes - will mature into a progress bar as app proceeds
+
+
